3a0a5e22f54c9cc0b0b00f3e713d00cd5a07f7b6
Michael van der Zel <michael.vanderzel@gmail.com>
2025-02-14 11:25:20 +0100

    moved content

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# phrsfm-ig
 PHR-S Functional Model R2 to FHIR Requirements-based representation and publication with the Common HL7 Toolset (FHIR IG)
+
+## Running the scipt and IG publisher
+
+# Convert script
+```
+> docker run --name=phrsfm-ig -it -v "$(pwd)":/app node:latest /bin/bash
+@> (once) dpkg -i jdk-23_linux-x64_bin.deb
+@> (once) apt -y update; apt -y install graphviz jekyll
+@> (once) git config --global --add safe.directory /app
+@> cd script
+@> (once) npm install
+@> node max2fhir.js
+@> node max2plantuml.js > ../input/images-source/relationships.plantuml 
+```
+
+Update groupings and resources from output.txt in phrs-ig.json
+
+# To build IG
+```
+(optional)> curl -L https://github.com/HL7/fhir-ig-publisher/releases/latest/download/publisher.jar -o input-cache/publisher.jar
+@> java -jar input-cache/publisher.jar -ig ig.ini
+```
---
+++ script/max2fhir.js
@@ -0,0 +1,398 @@
+const fs = require('fs'),
+    xml2js = require('xml2js');
+const xml = new xml2js.Parser();
+//
+// If you change the CANONICAL remember to manually change the CANONICAL in the input/profiles files!
+// Also update ig.ini when PACKAGE_ID changes
+//
+const MAX_FILE = "phrs-fm.max";
+const CANONICAL = "http://hl7.org/ehrs/uv/phrsfmr2";
+const BASE_CANONICAL = "http://hl7.org/ehrs/uv/ehrs-base";
+const PACKAGE_ID = "hl7.ehrs.uv.phrsfmr2";
+const FMID_PREFIX = "PHRSFMR2";
+
+var maxroot;
+xml.parseString(fs.readFileSync(MAX_FILE), function (err, data) {
+    maxroot = data;
+});
+// The order in the base model is wrong in some places e.g. RI.1.1.25 criteria come before the function.
+// Fix the order.
+sort(maxroot);
+
+var rawSatisfiedBy = fs.readFileSync("satisfiedBy.txt").toString();
+var satisfiedBy = {};
+rawSatisfiedBy.split('\n').forEach(row => {
+    var idx = row.indexOf(',');
+    var id = row.substring(0, idx);
+    var uri = row.substring(idx+1);
+    satisfiedBy[id] = uri;
+});
+
+var groupings = [ ];
+var resources = [ ];
+var reqs = { };
+
+maxroot.model.objects.forEach(_temp => {
+    _temp.object.forEach(object => {
+        var parentObject;
+        var fhir_parent_req;
+        if (object.parentId) {
+            parentObject = _temp.object.find(object2 => object2.id[0] == object.parentId[0]);
+            fhir_parent_req = reqs[object.parentId[0]];
+        }
+
+        var fhir_req;
+        switch (object.stereotype[0]) {
+            case "HL7-FM":
+                fhir_req = handleFM(object, parentObject);
+                break;
+            case "Section":
+                fhir_req = handleSection(object, parentObject);
+                break;
+            case "Header":
+            case "Function":
+                fhir_req = handleHeaderOrFunction(object, parentObject);
+                break;
+            case "Criteria":
+                handleCriteria(object, fhir_parent_req);
+                break;
+            default:
+                break;
+        }
+        if (fhir_req) {
+            reqs[object.id[0]] = fhir_req;
+        }
+    });
+});
+
+Object.values(reqs).forEach(req => {
+    var filename = "../input/resources/Requirements-" + req.id + ".json";
+    console.log(filename);
+    // console.log(JSON.stringify(req, null, 2));
+    fs.writeFileSync(filename, JSON.stringify(req, null, 2))
+});
+
+sortResources();
+var grouping = {
+    "id": "OTH",
+    "name" : "Other",
+    "description" : ""
+}
+groupings.push(grouping);
+
+// create ehrsfm-ig.json from template
+var template_filename = "ig-template.json";
+var ig_json = fs.readFileSync(template_filename, 'utf8');
+ig_json = ig_json.replace("$GROUPINGS$", JSON.stringify(groupings, null, 2));
+ig_json = ig_json.replace("$RESOURCES$", JSON.stringify(resources, null, 2));
+ig_json = ig_json.replaceAll("$CANONICAL$", CANONICAL);
+ig_json = ig_json.replaceAll("$PACKAGE_ID$", PACKAGE_ID);
+
+var filename = `../input/ImplementationGuide-${PACKAGE_ID}.json`;
+fs.writeFileSync(filename, ig_json);
+
+function handleFM(fm) {
+    var title = fm.name[0];
+    var name = title.replace(/[^A-Za-z0-9_]/g, '_');
+    // var notes = fm.notes[0];
+
+    var fhir_req = {
+        "resourceType": "Requirements",
+        "id": FMID_PREFIX,
+        "meta": {
+            "profile": [
+                `${BASE_CANONICAL}/StructureDefinition/FunctionalModel`
+            ]
+        },
+        // "url": `${BASE}/Requirements/` + fmid,
+        "name": name,
+        "title": name,
+        "status": "active"
+    };
+
+    var resource = {
+        "reference": { "reference": `Requirements/${FMID_PREFIX}` },
+        "isExample": false,
+        "groupingId": "OTH"
+    }
+    resources.push(resource);
+
+    return fhir_req;
+}
+
+function handleSection(section, parentObject) {
+    var title = section.name[0];
+    // Names should be computer friendly
+    var name = title.replace(/[^A-Za-z0-9_]/g, '_');
+    var alias = section.alias[0];
+    var notes = section.notes[0];
+    var ovidx = notes.indexOf("$OV$"); // always 4
+    var exidx = notes.indexOf("$EX$");
+    var acidx = notes.indexOf("$AC$");
+    var overview = notes.substring(4, exidx).trim();
+    var example = notes.substring(exidx+4, acidx).trim();
+    var actors = notes.substring(acidx+4).trim();
+    var _description = overview + (example!=""?`\nExample:\n${example}`:"");
+
+    var fhir_section = {
+        "resourceType": "Requirements",
+        "id": `${FMID_PREFIX}-${alias}`,
+        "meta": {
+            "profile": [
+                `${BASE_CANONICAL}/StructureDefinition/FMSection`
+            ]
+        },
+        "extension": [
+            {
+                "url": `${BASE_CANONICAL}/StructureDefinition/requirements-actors`,
+                "valueString": actors
+            }
+        ],
+        // "url": "http://hl7.org/fhir/Requirements/" + fmid + "-" + alias,
+        "name": name,
+        "title": title,
+        "status": "active",
+        "description": _description
+    }
+    // remove extension if there is no example
+    if (actors == "") {
+        delete fhir_section.extension;
+    }
+    var grouping = {
+        "id": alias,
+        "name" : title + " Section",
+        "description" : overview
+    }
+    groupings.push(grouping);
+
+    var resource = {
+        "reference": { "reference": `Requirements/${FMID_PREFIX}-${alias}` },
+        "isExample": false,
+        "groupingId": "OTH"
+    }
+    resources.push(resource);
+ 
+    return fhir_section;
+}
+
+function handleHeaderOrFunction(headerOrFunction, parentObject) {
+    // some assertions
+    if (headerOrFunction.notes == undefined) {
+        console.error("HeaderOrFunction without notes? " + JSON.stringify(headerOrFunction));
+        return;
+    }
+
+    // There are names with a '&', that gives issues in Jekyll? In title they need to be html escaped.
+    var title = headerOrFunction.name[0];
+    // Names should be computer friendly
+    var name = headerOrFunction.name[0].replace(/[^A-Za-z0-9_]/g, '_');
+    var alias = headerOrFunction.alias[0];
+    var notes = headerOrFunction.notes[0];
+    var stidx = notes.indexOf("$ST$");
+    var deidx = notes.indexOf("$DE$");
+    var exidx = notes.indexOf("$EX$");
+    if (exidx == -1) exidx = notes.length;
+    var statement = notes.substring(4, deidx).trim();
+    var description = notes.substring(deidx+4, exidx).trim();
+    var example = notes.substring(exidx+4).trim();
+    var type = headerOrFunction.stereotype[0];
+    var _description = description + (example!=""?`\nExample:\n${example}`:"");
+
+    var fhir_headerorfunction = {
+        "resourceType": "Requirements",
+        "id": `${FMID_PREFIX}-${alias}`,
+        "meta": {
+            "profile": [
+                `${BASE_CANONICAL}/StructureDefinition/FM${type}`
+            ]
+        },
+        // "url": `http://hl7.org/fhir/Requirements/${fmid}-${alias}`,
+        "name": name,
+        "title": `${title} (${type})`,
+        "status": "active",
+        "description": statement,
+        "purpose": _description
+    }
+    var resource = {
+        "reference": { "reference": `Requirements/${FMID_PREFIX}-${alias}` },
+        "isExample": false,
+        "groupingId": alias.substring(0, alias.indexOf('.'))
+    }
+    resources.push(resource);
+
+    return fhir_headerorfunction;
+}
+
+function handleCriteria(criteria, fhir_parent_req) {
+    var id = criteria.name[0];
+    var name = criteria.name[0];
+    var notes = criteria.notes[0];
+    var optionality = criteria.tag.find(tag => tag['$'].name === 'Optionality');
+    var conditional = criteria.tag.find(tag => tag['$'].name === 'Conditional');
+    var dependent = criteria.tag.find(tag => tag['$'].name === 'Dependent');
+    var refAlias = criteria.tag.find(tag => tag['$'].name === 'Reference.Alias');
+    var refFunctionID = criteria.tag.find(tag => tag['$'].name === 'Reference.FunctionID');
+    var refCriteriaID = criteria.tag.find(tag => tag['$'].name === 'Reference.CriteriaID');
+
+    // replace special link notation '[[{id}]]' with markdown link
+    notes = notes.replace(/\[\[([^\]]+)\]\]/g, '[\$1](Requirements-EHRSFMR2-\$1.html)');
+
+    if (!fhir_parent_req) {
+        console.log("parent not yet created? " + name);
+        return;
+    }
+    var fhir_statement = {
+        "extension": [
+            {
+                "url": `${BASE_CANONICAL}/StructureDefinition/requirements-dependent`,
+                "valueBoolean": (dependent['$'].value=="Y")
+            }
+        ],
+        "key": FMID_PREFIX + "-" + id.replace('#','-'),
+        "label": name,
+        "conformance": [
+          optionality['$'].value
+        ],
+        "conditionality": (conditional['$'].value=="Y"),
+        "requirement": notes
+    };
+    if (!fhir_parent_req.statement) { fhir_parent_req.statement = [] }
+    if (refAlias && refAlias['$'].value != "" && refFunctionID && refFunctionID['$'].value != "") {
+        fhir_statement["derivedFrom"] = refAlias['$'].value + " " + refFunctionID['$'].value + (refCriteriaID?"#" + refCriteriaID['$'].value:"");
+    }
+    if (id in satisfiedBy) { fhir_statement["satisfiedBy"] = [ satisfiedBy[id]] }
+    fhir_parent_req.statement.push(fhir_statement);
+}
+
+function sort(obj) {
+    // sort by FM ID
+    var section_sortkey = [];
+    // EHR-S FM
+    section_sortkey['OV'] = '1OV';
+    section_sortkey['CP'] = '2CP';
+    section_sortkey['CPS'] = '3CPS';
+    section_sortkey['AS'] = '4AS';
+    section_sortkey['POP'] = '5POP';
+    section_sortkey['RI'] = '6RI';
+    section_sortkey['TI'] = '7TI';
+    // extra UFP chapter
+    section_sortkey['U'] = '8U';    
+    // extra PHR-S FM chapters
+    section_sortkey['PH'] = '1PH';
+    section_sortkey['S'] = '2S';
+
+    obj['model'].objects[0].object.sort(function (a,b) {
+        var aname = a.name;
+        var bname = b.name;
+        // Package should be first!
+        if (a.stereotype == 'HL7-FM-ProfileDefinition' || a.stereotype == 'HL7-FM-Profile' || a.stereotype == 'HL7-FM') {
+            aname = "0" + aname;
+        }
+        if (a.stereotype == 'HL7-FM-ProfileDefinition' || b.stereotype == 'HL7-FM-Profile' || a.stereotype == 'HL7-FM') {
+            bname = "0" + bname;
+        }
+
+        if (a.stereotype == 'Section') {
+            aname = section_sortkey[a.alias];
+        }
+        else if (a.stereotype == 'Header' || a.stereotype == 'Function') {
+            var p = a.alias[0].split(/[\.]/);
+            aname = section_sortkey[p[0]];
+            for(var i=1; i<p.length; i++){
+                aname += '.';
+                var num = Number(p[i]);
+                if (num<10) aname += '0';
+                aname += num;
+            }
+        }
+        else if (a.stereotype == 'Criteria') {
+            var p = a.name[0].split(/[\.#]/);
+            aname = section_sortkey[p[0]];
+            for(var i=1; i<p.length-1; i++){
+                aname += '.';
+                var num = Number(p[i]);
+                if (num<10) aname += '0';
+                aname += num;
+            }
+            aname += '#';
+            var cnum = Number(p[p.length-1]);
+            if (cnum<10) aname += '00';
+            else if (cnum<100) aname += '0';
+            aname += cnum;
+        }
+
+        if (b.stereotype == 'Section') {
+            bname = section_sortkey[b.alias];
+        }
+        else if (b.stereotype == 'Header' || b.stereotype == 'Function') {
+            var p = b.alias[0].split(/[\.]/);
+            bname = section_sortkey[p[0]];
+            for(var i=1; i<p.length; i++){
+                bname += '.';
+                if (p[i]<10) bname += '0';
+                bname += p[i];
+            }
+        }
+        else if (b.stereotype == 'Criteria') {
+            var p = b.name[0].split(/[\.#]/);
+            bname = section_sortkey[p[0]];
+            for(var i=1; i<p.length-1; i++){
+                bname += '.';
+                var num = Number(p[i]);
+                if (num<10) bname += '0';
+                bname += num;
+            }
+            bname += '#';
+            var cnum = Number(p[p.length-1]);
+            if (cnum<10) bname += '00';
+            else if (cnum<100) bname += '0';
+            bname += cnum;
+        }
+        // console.log(aname + " " + bname);
+        return (aname > bname) ? 1 : -1 ;
+    });    
+}
+
+function sortResources(obj) {
+    // sort by FM ID
+    var section_sortkey = [];
+    // EHR-S FM and UFP chapters
+    section_sortkey['OV'] = '1OV';
+    section_sortkey['CP'] = '2CP';
+    section_sortkey['CPS'] = '3CPS';
+    section_sortkey['AS'] = '4AS';
+    section_sortkey['POP'] = '5POP';
+    section_sortkey['RI'] = '6RI';
+    section_sortkey['TI'] = '7TI';
+    // extra UFP chapter
+    section_sortkey['U'] = '8U';    
+    // extra PHR-S FM chapters
+    section_sortkey['PH'] = '1PH';
+    section_sortkey['S'] = '2S';
+
+    resources.sort(function (a,b) {
+        var aref = a.reference.reference;
+        var bref = b.reference.reference;
+        var aname = aref.substring(aref.indexOf('-')+1);
+        var bname = bref.substring(bref.indexOf('-')+1);
+
+        var p = aname.split(/[\.]/);
+        aname = section_sortkey[p[0]];
+        for(var i=1; i<p.length; i++){
+            aname += '.';
+            var num = Number(p[i]);
+            if (num<10) aname += '0';
+            aname += num;
+        }
+
+        var p = bname.split(/[\.]/);
+        bname = section_sortkey[p[0]];
+        for(var i=1; i<p.length; i++){
+            bname += '.';
+            if (p[i]<10) bname += '0';
+            bname += p[i];
+        }
+        //console.log(aname + " " + bname);
+        return (aname > bname) ? 1 : -1 ;
+    });    
+}
